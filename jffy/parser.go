fcd55f229a5e60a00dab6293499e990448fa0f25
Chris <4070267+Chris-Holdt@users.noreply.github.com>
2023-12-20 20:29:00 +0000

    feat: Improved parser error handling
    * On error panic, unwind to recover in main function call, handle

--- jffy/parser.go
+++ jffy/parser.go
@@ -2,8 +2,9 @@ package jffy
 
 import "fmt"
 
-type ParseError interface {
-	error
+type parseError struct {
+	operator IToken
+	msg      string
 }
 
 type IParser interface {
@@ -28,49 +29,50 @@ func Parser(tokens []IToken, jffy Jffy) IParser {
 }
 
 func (p *parser) Parse() IExpr {
-	expr, err := p.expression()
 
-	if err != nil {
-		return nil
+	// Catch panics
+	defer func() {
+		if r := recover(); r != nil {
+			resp := r.(parseError)
+			p.jffy.Error(resp.operator, resp.msg)
+		}
+	}()
+
+	expr := p.expression()
+
+	return expr
+}
+
+func (p *parser) expression() IExpr {
+	return p.equality()
+}
+
+func (p *parser) equality() IExpr {
+	expr := p.comparison()
+
+	for p.match(BANG_EQUAL, EQUAL_EQUAL) {
+		operator := p.previous()
+		right := p.comparison()
+		expr = &Binary{
+			expr,
+			operator,
+			right,
+		}
+
 	}
 
 	return expr
 }
 
-func (p *parser) expression() (IExpr, ParseError) {
-	return p.equality()
-}
-
-func (p *parser) equality() (IExpr, ParseError) {
-	var expr IExpr
-	var err ParseError
-	expr, err = p.comparison()
-
-	for p.match(BANG_EQUAL, EQUAL_EQUAL) {
-		operator := p.previous()
-		var right IExpr
-		right, err = p.comparison()
-		expr = &Binary{
-			expr,
-			operator,
-			right,
-		}
-
-	}
-
-	return expr, err
-}
-
-func (p *parser) comparison() (IExpr, ParseError) {
+func (p *parser) comparison() IExpr {
 
 	var expr IExpr
-	var err ParseError
-	expr, err = p.term()
+	expr = p.term()
 
 	for p.match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL) {
 		operator := p.previous()
-		var right IExpr
-		right, err = p.term()
+		right := p.term()
+
 		expr = &Binary{
 			expr,
 			operator,
@@ -78,20 +80,16 @@ func (p *parser) comparison() (IExpr, ParseError) {
 		}
 	}
 
-	return expr, err
+	return expr
 }
 
-func (p *parser) term() (IExpr, ParseError) {
-	var expr IExpr
-	var err ParseError
-
-	expr, err = p.factor()
+func (p *parser) term() IExpr {
+	expr := p.factor()
 
 	for p.match(MINUS, PLUS) {
 		operator := p.previous()
+		right := p.factor()
 
-		var right IExpr
-		right, err = p.factor()
 		expr = &Binary{
 			expr,
 			operator,
@@ -99,19 +97,15 @@ func (p *parser) term() (IExpr, ParseError) {
 		}
 	}
 
-	return expr, err
+	return expr
 }
 
-func (p *parser) factor() (IExpr, ParseError) {
-	var expr IExpr
-	var err ParseError
-	expr, err = p.unary()
+func (p *parser) factor() IExpr {
+	expr := p.unary()
 
 	for p.match(SLASH, STAR) {
 		operator := p.previous()
-
-		var right IExpr
-		right, err = p.unary()
+		right := p.unary()
 
 		expr = &Binary{
 			expr,
@@ -120,86 +114,85 @@ func (p *parser) factor() (IExpr, ParseError) {
 		}
 	}
 
-	return expr, err
+	return expr
 }
 
-func (p *parser) unary() (IExpr, ParseError) {
+func (p *parser) unary() IExpr {
 	if p.match(BANG, MINUS) {
 		operator := p.previous()
-		right, err := p.unary()
+		right := p.unary()
+
 		return &Unary{
-				operator,
-				right,
-			},
-			err
+			operator,
+			right,
+		}
 	}
 
 	return p.primary()
 }
 
-func (p *parser) primary() (IExpr, ParseError) {
+func (p *parser) primary() IExpr {
 	if p.match(FALSE) {
 		return &Literal{
-				false,
-			},
-			nil
+			false,
+		}
 	}
 
 	if p.match(TRUE) {
 		return &Literal{
-				true,
-			},
-			nil
+			true,
+		}
 
 	}
 
 	if p.match(NIL) {
 		return &Literal{
-				nil,
-			},
-			nil
+			nil,
+		}
 
 	}
 
 	if p.match(NUMBER, STRING) {
 		prev := p.previous()
 		return &Literal{
-				prev.Literal(),
-			},
-			nil
+			prev.Literal(),
+		}
 
 	}
 
 	if p.match(LEFT_PAREN) {
-		var expr IExpr
-		var err ParseError
-		expr, err = p.expression()
+		expr := p.expression()
 
 		p.consume(RIGHT_PAREN, "Expect \")\" after expression.")
 		return &Grouping{
-				expr,
-			},
-			err
-
+			expr,
+		}
 	}
 
-	return nil, p.error(p.peek(), "Expect Expression")
+	p.error(p.peek(), "Expect Expression")
+	return nil
 }
 
-func (p *parser) consume(tType TokenType, msg string) (IToken, ParseError) {
+func (p *parser) consume(tType TokenType, msg string) {
 	if p.check(tType) {
-		return p.advance(), nil
+		p.advance()
+		return
 	}
 
 	token := p.peek()
-
-	return nil, p.error(token, msg)
+	p.error(token, msg)
 }
 
-func (p *parser) error(token IToken, msg string) ParseError {
+func (p *parser) error(token IToken, msg string) {
 	p.jffy.Error(token, msg)
 
-	return nil
+	e := &parseError{
+		operator: token,
+		msg:      msg,
+	}
+
+	// Unwind to recover in Parse()
+	panic(e)
 }
 
 func (p *parser) match(types ...TokenType) bool {
